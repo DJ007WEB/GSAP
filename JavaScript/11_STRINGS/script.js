17ab51d349246bdb0b739c3fbd14eafaf341c2a5
DJ007WEB <007officialjyo@gmail.com>
2023-09-10 18:36:47 +0530

    String 1

---
+++ JavaScript/11_STRINGS/script.js
@@ -0,0 +1,26 @@
+
+/*
+
+indexOf();
+lastIndexOf();
+slice();
+
+toLowerCase();
+toUpperCase();
+
+trim();
+
+replace('kake replace korte hobe; , 'replace korar por sei jaiga te ki thakbe')
+replaceAll();
+
+
+startsWith(); All of these return boolean values.
+includes();
+endsWith();
+
+*/
+
+
+const horibol = 'Horibol Horibol Horibol Horibol Horibol'
+
+console.log(horibol.replaceAll('Horibol', 'Jai Shree Ram'));

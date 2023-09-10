efd2bef54d98bf43b4df3044458d0d1604f4eb85
DJ007WEB <007officialjyo@gmail.com>
2023-09-10 18:09:17 +0530

    updated

--- JavaScript/10_MAP/script.js
+++ JavaScript/10_MAP/script.js
@@ -7,9 +7,9 @@ const rest = new Map();
 
 rest.set('name','Jyotirmoy Dutta').set(1, 'Jph').set(2, 'Bankura').set(true, ' I can');
 
-console.log(rest);
+// console.log(rest);
 
-console.log(rest.get('name'));
+// console.log(rest.get('name'));
 
 
 const question = new Map([
@@ -23,18 +23,53 @@ const question = new Map([
   ]);
 
 
-  console.log(question.get('question'));
+//   console.log(question.get('question'));
 
-  for(const [key, val] of question) {
-    if(key === 'number') {
-        console.log(`Answer ${key}: ${val}`);
-    }
+//   for(const [key, val] of question) {
+//     if(key === 'number') {
+//         console.log(`Answer ${key}: ${val}`);
+//     }
+// }
+
+// const reply = Number(prompt('What is Your answer?'));
+
+// if(reply === 3) {
+//     console.log(`Answer is ${question.get(true)}`);
+// } else {
+//     console.log(`${question.get(false)}`);
+// }
+
+
+const gameEvents = new Map([
+    [17, '⚽️ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽️ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽️ GOAL'],
+    [80, '⚽️ GOAL'],
+    [92, '🔶 Yellow card'],
+  ]);
+
+
+  const events = new Set([...gameEvents.values()]);
+
+  console.log(events);
+
+  gameEvents.delete(64);
+
+  console.log(gameEvents);
+
+
+  const time = [...gameEvents.keys()].pop();
+
+  console.log(`An event happen on an average of ${time/gameEvents.size}`);
+
+
+  for(const [min, event] of gameEvents) {
+    console.log(`${min <= 45 ? 'FIRST' : 'SECOND'} HALF : ${event}`);
 }
 
-const reply = Number(prompt('What is Your answer?'));
-
-if(reply === 3) {
-    console.log(`Answer is ${question.get(true)}`);
-} else {
-    console.log(`${question.get(false)}`);
-}

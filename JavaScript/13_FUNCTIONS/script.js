8a578a211b9db1d02c94d6f56ebff41555e07577
DJ007WEB <007officialjyo@gmail.com>
2023-09-11 11:39:15 +0530

    updated

--- JavaScript/13_FUNCTIONS/script.js
+++ JavaScript/13_FUNCTIONS/script.js
@@ -40,5 +40,33 @@ const greet = (greeting) => {
 
 const greetHey = greet('hey');
 
-greetHey('milan')
+greetHey('milan');
+
+
+const airIndia = {
+    airline : 'Air India',
+    code : 'AI',
+    bookings : [],
+    book(flightNum, name) {
+        console.log(`${name} has booked a seat on ${this.airline} and the seat no is ${this.code}${flightNum}`);
+        this.bookings.push(`{${this.code}${flightNum}, ${name}}`)
+    },
+}
+
+airIndia.book(158, 'Milan');
+
+console.log(airIndia.bookings);
+
+
+const kingFisher = {
+    airline : 'king Fisher',
+    code : 'KF',
+    bookings : []
+}
+
+
+const book = airIndia.book;
+
+book.call(kingFisher, 456, 'jYo');
+console.log(kingFisher);
 

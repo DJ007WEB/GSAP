f72361d3a6a98ce002aaa6810c50cc64c3b007e5
DJ007WEB <007officialjyo@gmail.com>
2023-09-27 15:54:18 +0530

    Done

--- JavaScript/29_CODING_CHALLANGE 9/script.js
+++ JavaScript/29_CODING_CHALLANGE 9/script.js
@@ -3,8 +3,27 @@
 
 
 class Car {
-    constructor(name,spped) {
+    constructor(name,speed) {
         this.name = name;
-        this.spped = spped;
+        this.speed = speed;
+    }
+
+    get speedUs() {
+        return this.speed/1.6;
+    }
+
+    set speedUs(sp) {
+        this.speed = sp*1.6;
     }
 }
+
+const  bmw = new Car('BMW',130);
+
+console.log(bmw);
+
+bmw.speedUs = 50;
+
+console.log(bmw);
+
+
+

6a0bf75774035a17d27b4953bc4bfdbad5fad7bf
DJ007WEB <007officialjyo@gmail.com>
2023-10-05 14:54:34 +0530

    Dates Done

--- JavaScript/19_BANKIST_APP/script2.js
+++ JavaScript/19_BANKIST_APP/script2.js
@@ -1,10 +1,22 @@
 "use strict";
-
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -12,23 +24,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -78,21 +89,48 @@ userName(accounts);
 const displayMoveBalSummary = function (acc, sorting = false) {
   containerMovements.innerHTML = "";
 
+  // GETTING THE DATE OF THE LOG IN TIME
+
+  const loginDate = new Date();
+  const date = `${loginDate.getDate()}`.padStart(2, 0);
+  const month = `${loginDate.getMonth()+1}`.padStart(2, 0);
+  const year = loginDate.getFullYear();
+
+  // GETTING THE TIME OF LOG IN 
+
+  console.log(loginDate);
+
+  const hour = `${loginDate.getHours()}`.padStart(2,0);
+  const min = `${loginDate.getMinutes()}`.padStart(2,0);
+
+  labelDate.textContent = `${date}/${month}/${year}, ${hour}:${min}`;
+
   // SORTING
 
   const sortedMov = sorting
     ? acc.movements.slice().sort((a, b) => a - b)
     : acc.movements;
 
+  // const soertedDate = sorting ? acc.movementsDates.slice().sort((a,b) => a-b) : acc.movementsDates;
+
+  // console.log(soertedDate, sorting);
+
   // DISPLAYING THE MOVEMENTS
   sortedMov.forEach((mov, i) => {
     const type = mov > 0 ? "deposit" : "withdrawal";
+
+    // GETTING THE DETAILS OF CORRESPONDING DATES OF THE TRANSACTIONS
+    const currDate = new Date(acc.movementsDates[i]);
+    const date = `${currDate.getDate()}`.padStart(2, 0);
+    const month = `${currDate.getMonth()+1}`.padStart(2, 0);
+    const year = currDate.getFullYear();
 
     const html = `
       <div class="movements__row">
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+    <div class="movements__date">${date}/${month}/${year}</div>
       <div class="movements__value">${mov}€</div>
     </div>`;
 
@@ -117,7 +155,7 @@ const displayMoveBalSummary = function (acc, sorting = false) {
     .filter((mov) => mov < 0)
     .reduce((acc, curr) => acc + curr, 0);
 
-  labelSumOut.innerHTML = `${withdrawal}€`;
+  labelSumOut.innerHTML = `${Math.floor(withdrawal)}€`;
 
   // INTEREST
   const interest = acc.movements
@@ -126,7 +164,7 @@ const displayMoveBalSummary = function (acc, sorting = false) {
     .filter((int) => int >= 1)
     .reduce((acc, curr) => acc + curr, 0);
 
-  labelSumInterest.innerHTML = `${interest}€`;
+  labelSumInterest.innerHTML = `${interest.toFixed(2)}€`;
 };
 
 // 3 IMPLEMENTING LOG IN
@@ -173,6 +211,9 @@ const transferMoney = function (e) {
   ) {
     currAccount.movements.push(-transAmount);
     receiverAcc.movements.push(transAmount);
+    // PUSHING THE DATE AND TIME OF THE TRANSACTIONS
+    currAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
 
     displayMoveBalSummary(currAccount);
 
@@ -189,6 +230,8 @@ const loanReq = function (e) {
 
   if (currAccount.movements.some((mov) => mov > loanAmo * 0.1)) {
     currAccount.movements.push(loanAmo);
+    // PUSHING THE DATE AND TIME OF THE TRANSACTIONS
+    currAccount.movementsDates.push(new Date().toISOString());
     displayMoveBalSummary(currAccount);
 
     inputLoanAmount.value = "";

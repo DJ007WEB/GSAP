e3cffb1c3d7cb4828573575ad8ad27d4202b9427
DJ007WEB <007officialjyo@gmail.com>
2023-12-17 11:32:08 +0530

    Star Rating Practice

--- REACT/05.usepopcorn/src/index.js
+++ REACT/05.usepopcorn/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
+import StarRating2 from "./StarRating2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    {/* <StarRating maxRating={10} /> */}
+    <StarRating2 maxRating={10} />
   </React.StrictMode>
 );

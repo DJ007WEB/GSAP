e4d49f2fa00f219a7c82ca1dc0acbdc7543a0841
DJ007WEB <007officialjyo@gmail.com>
2023-12-20 12:40:17 +0530

    Practice Done

--- REACT/05.usepopcorn/src/index.js
+++ REACT/05.usepopcorn/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 // import StarRating from "./StarRating";
 // import StarRating2 from "./StarRating2";
+import App from "./App-v2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

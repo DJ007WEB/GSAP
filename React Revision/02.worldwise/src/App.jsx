fb553b07e74e36f3f504c411fea44867aae240b0
DJ007WEB <007officialjyo@gmail.com>
2024-01-17 09:18:37 +0530

    Programmatic navigation done

--- React Revision/02.worldwise/src/App.jsx
+++ React Revision/02.worldwise/src/App.jsx
@@ -8,6 +8,8 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -48,10 +50,12 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>

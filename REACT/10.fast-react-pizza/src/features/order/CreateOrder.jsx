42d32882065d3c88f5eac1169eb1ae708e87feed
DJ007WEB <007officialjyo@gmail.com>
2024-01-24 11:31:55 +0530

    grid and button  done

--- REACT/10.fast-react-pizza/src/features/order/CreateOrder.jsx
+++ REACT/10.fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,31 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -40,7 +40,7 @@ function CreateOrder() {
 
   const formErrors = useActionData();
 
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
 
   return (
     <div>
@@ -80,8 +80,11 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" value={JSON.stringify(cart)} name="cart" />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Submitting..." : "Order now"}
+          <button
+            disabled={isSubmitting}
+            className="rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? 'Submitting...' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -96,7 +99,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   // console.log(order);
@@ -104,7 +107,7 @@ export async function action({ request }) {
   const errors = {};
 
   if (!isValidPhone(order.phone))
-    errors.phone = "Please give us your correct phone number.";
+    errors.phone = 'Please give us your correct phone number.';
 
   if (Object.keys(errors).length > 0) return errors;
 

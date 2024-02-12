eb2ef982d75df2c0a70c7374eb2bbac7aa819181
DJ007WEB <007officialjyo@gmail.com>
2024-02-12 15:37:29 +0530

    Client Side Filtering Done

--- REACT/11.the-wild-oasis/src/ui/Filter.jsx
+++ REACT/11.the-wild-oasis/src/ui/Filter.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/prop-types */
 import styled, { css } from "styled-components";
+
+import { useSearchParams } from "react-router-dom";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -33,3 +36,32 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+export default function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  console.log(options);
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => {
+        return (
+          <FilterButton
+            onClick={() => handleClick(option.value)}
+            key={option.label}
+            active={option.value === currentFilter}
+          >
+            {option.label}
+          </FilterButton>
+        );
+      })}
+    </StyledFilter>
+  );
+}
